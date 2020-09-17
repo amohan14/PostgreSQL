79a78c3fcdb54239a95307ea280f28b7a4bee90a
amohan14 <ananya.mhty@gmail.com>
2020-09-17 11:50:04 -0400

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -17,7 +17,8 @@ create_table () {
   echo "Create pgsql table and copy data to the table"
   # git clone the csv data to local
   sudo yum install git -y
-  sudo git clone https://github.com/amohan14/PostgreSQL.git /tmp
+  sudo cd /tmp
+  sudo git clone https://github.com/amohan14/PostgreSQL.git
   # Moving the csv data file to /tmp folder as all users(including postgres user) can access /tmp folder
   # sudo mv ~/PostgreSQL/persons.csv /tmp
   # Changing owner and access rights to the .csv file
@@ -30,8 +31,8 @@ create_table () {
   echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /var/lib/pgsql/12/data/pg_hba.conf
   # Creating table and copying csv data to the table
   # -i : login flag
-  sudo -u postgres -i psql -c "create table persons (id serial, first_name varchar(50), last_name varchar(50),dob date, email varchar(255), primary key (id) );"
-  sudo -u postgres -i psql -c "COPY persons(first_name, last_name, dob, email) FROM '/tmp/persons.csv' DELIMITER ',' CSV HEADER;"
+  sudo -u postgres -i psql -c "CREATE TABLE persons (id serial, first_name varchar(50), last_name varchar(50),dob date, email varchar(255), primary key (id) );"
+  sudo -u postgres -i psql -c "COPY persons(first_name, last_name, dob, email) FROM '/tmp/PostgreSQL/persons.csv' DELIMITER ',' CSV HEADER;"
   echo "Finished"
 }
 
